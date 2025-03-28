701441c273a8289a6bcbd691f9938bf0d63ff992
Attila Reterics <reterics.attila@gmail.com>
2025-03-28 18:38:11 +0100

    chore: update Note fields

--- frontend/src/Pages/Admin/Notes.tsx
+++ frontend/src/Pages/Admin/Notes.tsx
@@ -1,6 +1,29 @@
 import CrudManager from '../../components/CrudManager';
+import {useApi} from "../../hooks/useApi.ts";
 
 const Notes = () => {
+  const {data: projectsRaw, isLoading: projectsAreLoading} = useApi('projects');
+  const {data: clientsRaw, isLoading: clientsAreLoading} = useApi('clients');
+  const {data: tasksRaw, isLoading: tasksAreLoading} = useApi('tasks');
+
+  if (projectsAreLoading || clientsAreLoading || tasksAreLoading)
+    return <p>Loading...</p>;
+
+  const projects = projectsRaw.map((d) => ({
+    value: d.id,
+    label: d.name,
+  }));
+
+  const clients = clientsRaw.map((d) => ({
+    value: d.id,
+    label: d.name,
+  }));
+
+  const tasks = tasksRaw.map((d) => ({
+    value: d.id,
+    label: d.name,
+  }));
+
   return (
     <CrudManager
       title='Notes'
@@ -8,9 +31,9 @@ const Notes = () => {
       fields={[
         {key: 'title', label: 'Title', type: 'text'},
         {key: 'content', label: 'Content', type: 'text'},
-        {key: 'related_project_id', label: 'Project ID', type: 'number'},
-        {key: 'related_task_id', label: 'Task ID', type: 'number'},
-        {key: 'related_client_id', label: 'Client ID', type: 'number'},
+        {key: 'related_project_id', label: 'Project ID', type: 'select', editable: true, options: projects},
+        {key: 'related_task_id', label: 'Task ID', type: 'select', editable: true, options: tasks},
+        {key: 'related_client_id', label: 'Client ID', type: 'select', editable: true, options: clients},
       ]}
     />
   );
